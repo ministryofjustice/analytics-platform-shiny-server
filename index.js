46ac340f989969abad2c1fc41c4ec788ba57f79c
Ross Jones <ross.jones@digital.justice.gov.uk>
2021-10-29 14:53:14 +0100

    Add explanatory comments to the server
    Tries to add clarifying comments to the code to explain what is unclear.
    We are unlikely to refactor as we aim to retire this application.

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 #!/usr/bin/env node
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 
+
+/*
+ * Creates a process that is a running shiny app, and configures a
+ * websocket proxy. This proxy enables client websocket connections
+ * to by forwarded to the previously created shiny server.
+ *
+ * Once up and running, the shiny server (which is a child process)
+ * provides a websocket interface, where each websocket connection
+ * is considered a new session. This means that although there is
+ * only a single shiny process, it is able to handle more than one
+ * user.
+ *
+ * This server, by default listens on 0.0.0.0:9999, whilst the
+ * shinyServer listens, by default, on 127.0.0.1:7999. The
+ * expectation is that port 9999 is only accessible to the shiny
+ * server and not to external users.
+ */
+
+
 // Monkeypatch sockJs to allow custom headers
 // eslint-disable-next-line no-unused-vars,import/order
 const { Session } = require('./patchSession');
 
 const http = require('http');
 const sockjs = require('sockjs');
-const nodeStatic = require('node-static');
+const nodeStatic = require('@brettz9/node-static');
 const httpProxy = require('http-proxy');
 const WebSocket = require('ws');
 const debug = require('debug')('app');
@@ -16,11 +35,15 @@ const path = require('path');
 const { log } = require('winston');
 require('./logging');
 
-// 1. Echo sockJs server
+/*
+ * Creates a sockjs websocket proxy, telling it that we want
+ * to proxy websocket requests that come in on the __sockjs__
+ * path to the process running locally on 127.0.0.1:7999 -
+ * which is our shiny server.
+ */
 const sockjsOpts = {
   prefix: '.*/__sockjs__(/[no]=\\w+)?',
   log,
-
 };
 
 const proxy = httpProxy.createServer({
@@ -30,8 +53,13 @@ const proxy = httpProxy.createServer({
 
 const sockjsEcho = sockjs.createServer(sockjsOpts);
 
-// 1. Sockjs to Websocket bridge
 const messageQueue = [];
+
+/*
+ * When we receive a new connection, we will create a websocket to
+ * talk to Shiny. This web socket ensures that errors aside, data is
+ * sent when it is possible to send it, and enqueued otherwise.
+ */
 sockjsEcho.on('connection', (conn) => {
   const ws = new WebSocket(
     'ws://127.0.0.1:7999/websocket',
@@ -43,7 +71,8 @@ sockjsEcho.on('connection', (conn) => {
     log('error', err);
     conn.close();
   });
-  log('debug', 'new conn');
+
+  log('debug', 'new connection');
   ws.on('open', (_wsConn, _req) => {
     log('info', 'connected to shiny', { service: 'target->proxy' });
     ws.on('close', () => conn.close());
@@ -55,6 +84,7 @@ sockjsEcho.on('connection', (conn) => {
       ws.send(messageQueue.shift());
     }
   });
+
   conn.on('data', (message) => {
     log('debug', `${message}`, { service: 'proxy->target' });
     try {
@@ -70,7 +100,10 @@ sockjsEcho.on('connection', (conn) => {
 });
 
 
-// 2. Static files server
+/*
+ * Requests for resources that are not websocket requests are instead
+ * served from the `static` folder.
+ */
 const staticDirectory = new nodeStatic.Server(path.join(__dirname, './static'));
 
 const server = http.createServer();
@@ -88,6 +121,11 @@ server.addListener('request', (req, res) => {
   return true;
 });
 
+
+/*
+ * Handles an incorrect URL or error when performing the HTTP upgrade
+ * to a websocket.
+ */
 server.on('upgrade', (wsreq, socket, head) => {
   socket.on('error', (err) => {
     log('error', err);
@@ -110,6 +148,22 @@ proxy.on('error', (err) => {
 
 log('info', '> [*] Listening on 0.0.0.0:9999');
 
+
+/*
+ * Spawns a specific R process to run the shiny application.
+ * Currently this process will only ever call `runServer.R`
+ * in the R folder, meaning that Golem (which is launched
+ * differently) cannot currently be used.
+ *
+ * stdout/stderr and handled to wrap any output in JSON
+ * although no attention is paid as to whether the data
+ * being written is already JSON or not. This pushes the
+ * problem for logging further into our stack.
+ *
+ * TODO: If this did some level of discovery it might be
+ * possible to locate the golem required files and just
+ * handle them like normal shiny apps.
+ */
 const shiny = spawn(
   'Rscript',
   [path.join(__dirname, './R/runServer.R')],
@@ -125,9 +179,16 @@ shiny.stderr.on('data', (data) => {
   log('info', `${data}`, { service: 'shiny.stderr' });
 });
 
-// 3. The actual web-server
+/*
+ * Configures the HTTP server to listen on the configured port
+ * and accept connections on any interface.
+ */
 server.listen(process.env.PORT || 9999, '0.0.0.0');
 
+/*
+ * Handles the interupt signal to shutdown the http server and
+ * KILL the shiny process.
+ */
 process.on('SIGINT', () => {
   log('info', 'SIGINT signal received.');
   server.close(() => log('debug', 'Stopping proxy'));
